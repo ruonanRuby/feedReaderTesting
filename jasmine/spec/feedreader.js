ad617debe70b468d686ba72b72b848d6bc5be02b
ruonanRuby <rniu3@wisc.edu>
2019-08-05 14:36:56 -0500

    update several passed tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,36 +27,61 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* The test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('each contains legal url', function() {
+           for (let feed of allFeeds) {
+               expect(feed.url).toBeDefined();
+               expect(feed.url.length).not.toBe(0);
+           }
+        });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* The test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each contains legal name', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).toBeGreaterThan(0);
+            })
+        });
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    
+    describe('The menu', function() {
+        const menu = document.querySelector('body');
 
-        /* TODO: Write a test that ensures the menu element is
+        /* The test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('menu element hidden by default', function() {
+            expect(menu.classList).toContain('menu-hidden');
+        });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* The test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+
+
+    });
+
+
+        
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* TODO: Write a tes t that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
